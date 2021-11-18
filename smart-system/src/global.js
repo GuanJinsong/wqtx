11f970488f6da7eff227cd6bb86d0e088c7dea00
guanjingsong <171976685@qq.com>
2021-11-18 08:57:21 +0800

    修改1

--- smart-system/src/global.js
+++ smart-system/src/global.js
@@ -21,6 +21,9 @@ if (process.env.NODE_ENV === 'production') {
   // socketUrl = "ws://192.168.3.178:7070";    //定义实时通讯请求域名(本地服务器)
 
   
+
+
+  
   //本地测试访问3
   RequestUrl = "http://192.168.3.30:7070";   //全局通用ajax请求域名
   imgLocalhost = "http://192.168.3.30:7070/api/";   //定义图片路径(本地服务器)
